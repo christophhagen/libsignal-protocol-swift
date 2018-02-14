afebd9d9ff6d9016c3170b5d901b90d62330846a
Christoph Hagen <christoph@spacemasters.eu>
2018-02-14 13:48:54 +0100

    Initial Commit

---
+++ libsignal-protocol-swift/libsignal_protocol_swift.h
@@ -0,0 +1,19 @@
+//
+//  libsignal_protocol_swift.h
+//  libsignal-protocol-swift
+//
+//  Created by User on 14.02.18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libsignal_protocol_swift.
+FOUNDATION_EXPORT double libsignal_protocol_swiftVersionNumber;
+
+//! Project version string for libsignal_protocol_swift.
+FOUNDATION_EXPORT const unsigned char libsignal_protocol_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libsignal_protocol_swift/PublicHeader.h>
+
+
